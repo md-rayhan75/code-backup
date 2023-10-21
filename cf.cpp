fe2f64f4dffb757ecd740042aa2bcf38fc66ac52
Saif Rayhan <saif@pop-os.localdomain>
2023-10-21 18:55:27 +0600

    upload

--- cf.cpp
+++ cf.cpp
@@ -9,7 +9,39 @@ int main() {
 			s.erase(i);
 			flag = true;
 			break;
+		}#include <bits/stdc++.h>
+		
+		#define endl '\n'
+		#define sqr(x) (x) * (x)
+		#define gcd(x,y) __gcd(x,y)
+		#define lcm(x,y) ((x/gcd(x,y)) * y)
+		#define sz(x) (int)x.size()
+		#define all(x) (x).begin(),(x).end()
+		#define rall(x) (x).rbegin(),(x).rend()
+		
+		
+		using namespace std
+			using ll = long long;
+		using ld = long double;
+		using ull = unsigned long long;
+		
+		const ld PI = acos(-1.0);
+		const ll N = 2e5 + 5;
+		int tc = 1;
+		
+		void solve() {
+			
 		}
+		int main() {
+			ios_base::sync_with_stdio(false); cin.tie(nullptr);
+			int t = 1;
+			cin >> t;
+			while (t--) {
+				solve();
+			}
+			return 0;
+		}
+		
  
 	}
 	if (!flag) {
